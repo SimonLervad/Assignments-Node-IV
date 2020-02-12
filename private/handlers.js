b568e4bcfe848d347e8075087f2d6a29a7d3575b
Simon Lervad <lervad1996@gmail.com>
2020-02-12 10:33:44 +0100

    milepæl for sidste opgave

---
+++ private/handlers.js
@@ -0,0 +1,61 @@
+'use strict';
+/*
+ * handlers.js
+ * Requesthandlers to be called by the routing mechanism
+ */
+const fs = require("fs");                               // file system access
+const httpStatus = require("http-status-codes");        // http sc
+const lib = require("../private/libWebUtil");           // home grown utilities
+const experimental = require("../private/myTemplater"); // highly experimental template
+const filename = "private/userMsg.json";
+const querystring = require("querystring");
+
+const goError = function(res) {
+    res.writeHead(httpStatus.NOT_FOUND, {   // http page not found, 404
+        "Content-Type": "text/html; charset=utf-8"
+    });
+    res.write("<h1>404 Not Found</h1>");
+    res.end();
+};
+
+exports.getAndRespond = function(path, contentType, res) {
+    console.log(path);
+    if (fs.existsSync(path)) {              // does file exist, sync
+        fs.readFile(path, function(err, data) { // read
+            if (err) {                      // if read error
+                console.log("nml: " + err);           // inform server
+                goError(res);               // inform user
+                return;                     // back to caller
+            }
+            res.writeHead(httpStatus.OK, contentType); // prep header
+            res.write(data);                // prep body with read data
+            res.end();                      // send response
+        });
+    } else {
+        goError(res);                       // doesnt exist error
+    }
+};
+exports.receiveData = function(req, res, data) {
+    console.log("you went here");
+    let obj = lib.makeWebArrays(req, data);         // home made GET and POST objects
+    res.writeHead(httpStatus.OK, {                  // yes, write relevant header
+        "Content-Type": "text/html; charset=utf-8"
+    });
+    res.write(experimental.receipt(obj));           // home made templating for native node
+    let array = [];                                                // define array
+    fs.readFile(filename, "utf8", function(err1, info) {
+        if (err1) {
+            throw err1;
+        }
+        array = JSON.parse(info);                                           // new
+        array.push(obj);                                  // push obj onto array
+        let jsonstr = JSON.stringify(arr);              // stringify
+
+        fs.writeFile(filename, jsonstr, function(err) { // write to json file
+            if (err) {                                  // rewrite, not update
+                throw err;
+            }
+            console.log("Your profile was succesfully created");
+        });
+    });
+}
---
+++ routes/router.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const handlers = require("../private/handlers");    // handlers module
+const httpStatus = require("http-status-codes");
+const contentTypes = {
+    "text": { "Content-Type": "text/plain; charset=utf-8" },
+    "start": { "Content-Type": "text/html; charset=utf-8" },
+    "js": { "Content-Type": "application/js" },
+    "css": { "Content-Type": "text/css" },
+    "png": { "Content-Type": "image/png" },
+    "jpg": { "Content-Type": "image/jpg" },
+    "gif": { "Content-Type": "image/gif" },
+    "ico": { "Content-Type": "image/x-icon" },
+    "svg": { "Content-Type": "image/svg+xml" }
+};
+
+const routes = {                                    // register handles to routes
+    "GET": {
+        "/start": handlers.getAndRespond,
+        "/side": handlers.getAndRespond,
+        "/about": handlers.getAndRespond,
+        "/contact": handlers.getAndRespond,
+        "js": handlers.getAndRespond,
+        "css": handlers.getAndRespond,
+        "png": handlers.getAndRespond,
+        "jpg": handlers.getAndRespond,
+        "gif": handlers.getAndRespond,
+        "ico": handlers.getAndRespond,
+        "svg": handlers.getAndRespond
+    },
+
+    "POST": {
+        "/contact": handlers.receiveData
+    }
+};
+
+exports.route = function(req, res, body) {          // routing
+    let asset;
+    let type;
+    let routedUrl;
+    if (req.url.indexOf(".js") !== -1) {            // check for asset types
+        asset = "js";
+        routedUrl = "public/js" + req.url;
+        type = contentTypes.js;
+    } else if (req.url.indexOf(".css") !== -1) {
+        asset = "css";
+        routedUrl = "public/css" + req.url;
+        type = contentTypes.css;
+    } else if (req.url.indexOf(".png") !== -1) {
+        asset = "png";
+        routedUrl = "public/images" + req.url;
+        type = contentTypes.png;
+    } else if (req.url.indexOf(".jpg") !== -1) {
+        asset = "jpg";
+        routedUrl = "public/images" + req.url;
+        type = contentTypes.jpg;
+    } else if (req.url.indexOf(".gif") !== -1) {
+        asset = "gif";
+        routedUrl = "public/images" + req.url;
+        type = contentTypes.gif;
+    } else if (req.url.indexOf(".svg") !== -1) {
+        asset = "svg";
+        routedUrl = "public/images" + req.url;
+        type = contentTypes.svg;
+    } else if (req.url.indexOf(".ico") !== -1) {
+        asset = "ico";
+        routedUrl = req.url;
+        type = contentTypes.ico;
+    } else {
+        if (req.url.charAt(req.url.length - 1) === "/") {
+            asset = "/start";
+            routedUrl = "views/index.html";
+            type = contentTypes.html;
+        } else if (req.url === "/start") {
+            asset = req.url;
+            routedUrl = "views/index.html";
+            type = contentTypes.html;
+        } else {
+            asset = req.url;                            
+            if (req.method === "GET") {
+                routedUrl = "views" + req.url + ".html";    // if post use "req"
+                type = contentTypes.html;                   // if post use "res" 
+            } else {
+                routes[req.method][asset](req, res, body);
+                return;
+            }
+        }
+    }
+
+    try {
+        if (routes[req.method][asset]) {            // does handler exist to this route
+            console.log("routing: " + asset + " file: " + routedUrl);
+            routes[req.method][asset](routedUrl, type, res);  // yes, call it with params
+        } else if (routes[req.method]["/contact"]) {
+
+        } else {                                // no, return error msg
+            console.log("nmlnf: " + req.url);
+            res.writeHead(httpStatus.NOT_FOUND, contentTypes.text);
+            res.end(`route for <kbd>${req.url}</kbd> not found`);
+        }
+    } catch (ex) {                              // routing exception
+        console.log("Log: Routing exception: " + ex);
+    }
+};
